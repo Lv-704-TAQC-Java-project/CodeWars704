75c5af3d1ca95d6486e0d4a8f068c3736707db27
Jaroslav Tarasovych <staryk90@gmail.com>
2022-06-28 23:08:06 +0300

    twoDecimalPlaces has done!

--- src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/EigthImpl.java
@@ -2,6 +2,8 @@ package com.org.ita.kata.implementation.YaroslavTarasovych;
 
 import com.org.ita.kata.Eight;
 
+import java.text.DecimalFormat;
+
 public class EigthImpl implements Eight {
     @Override
     public int liters(double time) {
@@ -40,7 +42,9 @@ public class EigthImpl implements Eight {
 
     @Override
     public double twoDecimalPlaces(double number) {
-        return 0;
+        DecimalFormat decimalFormat = new DecimalFormat("#.##");
+        String result = decimalFormat.format(number);
+        return Double.valueOf(result);
     }
 
     @Override
