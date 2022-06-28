4d7746a88e4c8d7c45eccfcbab3795fc2c22103b
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-06-28 20:34:58 +0300

    Implementation of squareOrSquareRoot method

--- src/main/java/com/org/ita/kata/implementation/ykireyeva/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/ykireyeva/EigthImpl.java
@@ -2,6 +2,8 @@ package com.org.ita.kata.implementation.ykireyeva;
 
 import com.org.ita.kata.Eight;
 
+import java.util.Arrays;
+
 public class EigthImpl implements Eight {
     @Override
     public int liters(double time) {
@@ -20,31 +22,48 @@ public class EigthImpl implements Eight {
 
     @Override
     public int[] squareOrSquareRoot(int[] array) {
-        return new int[0];
+        int[] outputArray = new int[array.length];
+        for (int i = 0; i < array.length; i++) {
+            int num = array[i];
+            if (num % Math.sqrt(num) == 0) {
+                outputArray[i] = (int) Math.sqrt(num);
+            } else {
+                outputArray[i] = num * num;
+            }
+        }
+        return outputArray;
+
+        /*
+         * Implementation with Stream API
+         *
+         * return Arrays.stream(array)
+         *       .map(x -> (x % Math.sqrt(x) == 0) ? (int) Math.sqrt(x) : x * x)
+         *       .toArray();
+         */
     }
 
-    @Override
-    public int[] countPositivesSumNegatives(int[] input) {
-        return new int[0];
-    }
+        @Override
+        public int[] countPositivesSumNegatives ( int[] input){
+            return new int[0];
+        }
 
-    @Override
-    public int stringToNumber(String str) {
-        return 0;
-    }
+        @Override
+        public int stringToNumber (String str){
+            return 0;
+        }
 
-    @Override
-    public boolean amIWilson(double n) {
-        return false;
-    }
+        @Override
+        public boolean amIWilson ( double n){
+            return false;
+        }
 
-    @Override
-    public double twoDecimalPlaces(double number) {
-        return 0;
-    }
+        @Override
+        public double twoDecimalPlaces ( double number){
+            return 0;
+        }
 
-    @Override
-    public int[] divisibleBy(int[] numbers, int divider) {
-        return new int[0];
+        @Override
+        public int[] divisibleBy ( int[] numbers, int divider){
+            return new int[0];
+        }
     }
-}
