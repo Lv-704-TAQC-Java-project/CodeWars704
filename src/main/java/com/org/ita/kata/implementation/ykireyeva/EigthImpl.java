b73bfed9161f9801cf6070fb965a7c1f86f4240e
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-06-29 13:55:10 +0300

    Formatting the whole file

--- src/main/java/com/org/ita/kata/implementation/ykireyeva/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/ykireyeva/EigthImpl.java
@@ -27,28 +27,28 @@ public class EigthImpl implements Eight {
                 .toArray();
     }
 
-        @Override
-        public int[] countPositivesSumNegatives ( int[] input){
-            return new int[0];
-        }
-
-        @Override
-        public int stringToNumber (String str){
-            return 0;
-        }
-
-        @Override
-        public boolean amIWilson ( double n){
-            return false;
-        }
-
-        @Override
-        public double twoDecimalPlaces ( double number){
-            return 0;
-        }
-
-        @Override
-        public int[] divisibleBy ( int[] numbers, int divider){
-            return new int[0];
-        }
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
     }
+
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
