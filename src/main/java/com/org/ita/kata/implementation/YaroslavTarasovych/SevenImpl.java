fabc24e80d37562d879648d88ddc3e5ed819e890
Jaroslav Tarasovych <staryk90@gmail.com>
2022-06-29 19:43:12 +0300

    newAvg has done!

--- src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/SevenImpl.java
@@ -5,13 +5,13 @@ import com.org.ita.kata.Seven;
 public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] arr, double navg) {
-        double allSum=navg*(arr.length+1);
-        double sumArr=0;
-        for (int i = 0; i < arr.length; i++) {
-            sumArr+=arr[i];
+        double allSum = navg * (arr.length + 1);
+        double sumArr = 0;
+        for (double v : arr) {
+            sumArr += v;
         }
-        if (sumArr<allSum) {
-            return (long) Math.ceil((allSum-sumArr));
+        if (sumArr < allSum) {
+            return (long) Math.ceil((allSum - sumArr));
         } else throw new IllegalArgumentException();
 
     }
