c55cdbf8c86d8bf8afcdbf133a830515f3318fe3
Jaroslav Tarasovych <staryk90@gmail.com>
2022-06-29 20:29:37 +0300

    findNb has done!

--- src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/SIxImpl.java
+++ src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/SIxImpl.java
@@ -54,8 +54,4 @@ public class SIxImpl implements Six {
         return null;
     }
 
-    public static void main(String[] args) {
-        SIxImpl n=new SIxImpl();
-        System.out.println(n.findNb(4183059834009L));
-    }
 }
