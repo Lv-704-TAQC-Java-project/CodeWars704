d6d244d030e78ba661acfbbab280fa573b322a58
Jaroslav Tarasovych <staryk90@gmail.com>
2022-06-30 18:35:13 +0300

    balance has done!

--- src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/SIxImpl.java
+++ src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/SIxImpl.java
@@ -75,32 +75,4 @@ public class SIxImpl implements Six {
         return null;
     }
 
-    public static void main(String[] args) {
-        SIxImpl sIx = new SIxImpl();
-        System.out.println(sIx.balance("1864.00\n" +
-                "001 Video ;! 128.00 ?;\n" +
-                "002 Flowers ;! 12.22 ?;\n" +
-                "003 Video  34.00 \n" +
-                "004 Gasoline  13.65 \n" +
-                "005 Vegetables  93.50 \n" +
-                "006 Flowers ;! 12.00 ?;\n" +
-                "007 Books  3.20 \n" +
-                "008 Market ;! 13.65 ?;\n" +
-                "009 Grocery  93.50 \n" +
-                "010 Beauty  17.50"));
-    }
-//        String text = "002 Flowers ;! 12.22 ?;";
-//
-//        text = text.replaceAll("[;! \\?]", "");
-//        System.out.println(text);
-//        Pattern p1 = Pattern.compile("\\d+");
-//        Pattern p2 = Pattern.compile("[a-zA-Z]+");
-//        Pattern p3 = Pattern.compile("\\d+\\.\\d+");
-//        Matcher m1 = p1.matcher(text);
-//        Matcher m2 = p2.matcher(text);
-//        Matcher m3 = p3.matcher(text);
-//        if (m1.find()) System.out.println(text.substring(m1.start(), m1.end()));
-//        if (m2.find()) System.out.println(text.substring(m2.start(), m2.end()));
-//        if (m3.find()) System.out.println(text.substring(m3.start(), m3.end()));
-//    }
 }
