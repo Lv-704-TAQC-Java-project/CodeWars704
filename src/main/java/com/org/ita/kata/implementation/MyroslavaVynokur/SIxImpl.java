75d86c652b078d46e13e5955a1ab6c64983c509d
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-06-30 19:55:14 +0300

    add implementation balance

--- src/main/java/com/org/ita/kata/implementation/MyroslavaVynokur/SIxImpl.java
+++ src/main/java/com/org/ita/kata/implementation/MyroslavaVynokur/SIxImpl.java
@@ -2,6 +2,8 @@ package com.org.ita.kata.implementation.MyroslavaVynokur;
 
 import com.org.ita.kata.Six;
 
+import java.text.DecimalFormat;
+
 public class SIxImpl implements Six {
     @Override
     public long findNb(long m) {
@@ -10,7 +12,40 @@ public class SIxImpl implements Six {
 
     @Override
     public String balance(String book) {
-        return null;
+        final String ORIGINAL_BALANCE = "Original Balance: ";
+        final String TOTAL_EXPENSE = "Total expense  ";
+        final String AVERAGE_EXPENSE = "Average expense  ";
+        final String BALANCE = " Balance ";
+        final String ENTER = "\\r\\n";
+        final String SPACE = " ";
+
+        String cleanedBook = book.replaceAll("([^\\n. \\da-zA-Z])", "");
+        String[] lines = cleanedBook.split("[\\n]+");
+
+        double totalBalance = Double.parseDouble(lines[0]);
+        double totalExpense = 0;
+
+        StringBuilder sb = new StringBuilder();
+
+        String oneLine;
+        String[] oneLineArr;
+        for (int i = 1; i < lines.length; i++) {
+            oneLine = lines[i];
+            oneLineArr = oneLine.split("\\s+");
+            totalExpense += Double.parseDouble(oneLineArr[2]);
+            totalBalance -= Double.parseDouble(oneLineArr[2]);
+            sb.append(oneLineArr[0]).append(SPACE).append(oneLineArr[1]).append(SPACE).append(oneLineArr[2]).append(BALANCE)
+                    .append((new DecimalFormat("#.##").format(totalBalance))).append(ENTER);
+        }
+        double averageExpense = totalExpense / (lines.length - 1);
+        double averageExpenseF = Math.floor(averageExpense * 100 / 100);
+        double originalBalance = Double.parseDouble(lines[0]);
+
+        String completedString = sb.toString();
+
+        return ORIGINAL_BALANCE + (new DecimalFormat("#.00").format(originalBalance)) + ENTER + completedString +
+                TOTAL_EXPENSE + new DecimalFormat("0.00").format(totalExpense) +
+                ENTER + AVERAGE_EXPENSE + (new DecimalFormat("0.00").format(averageExpense));
     }
 
     @Override
