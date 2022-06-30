a351b56400f7b78b8981a59d78e82ff5c7dcf0db
Olenka <olenkadashinich@gmail.com>
2022-06-30 23:29:15 +0300

    Task 5 - gap

--- src/main/java/com/org/ita/kata/implementation/clarmmym/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/clarmmym/FiveImpl.java
@@ -3,6 +3,8 @@ package com.org.ita.kata.implementation.clarmmym;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FiveImpl implements Five {
     @Override
@@ -12,7 +14,37 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+        List<Long> primeNumbers = new ArrayList<>();
+
+        for (long i = m; i <= n; i++) {
+            if (isPrimeNumber(i)) {
+                primeNumbers.add(i);
+            }
+        }
+
+        long start = primeNumbers.get(0);
+        long end = primeNumbers.get(primeNumbers.size() - 1);
+        for (int i = 0; start != end; i++) {
+            long currentNumber = primeNumbers.get(i);
+            if (currentNumber - start == g) {
+                return new long[]{start, currentNumber};
+            } else {
+                start = currentNumber;
+            }
+        }
+        return null;
+    }
+
+    private boolean isPrimeNumber(long number) {
+        boolean result = true;
+        long controlNumber = (long) Math.sqrt(number);
+        for (long i = 2; i <= controlNumber; i++) {
+            if (number % i == 0) {
+                result = false;
+                break;
+            }
+        }
+        return result;
     }
 
     @Override
