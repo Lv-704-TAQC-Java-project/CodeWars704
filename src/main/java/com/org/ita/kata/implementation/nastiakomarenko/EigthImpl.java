5064ee40b6a40afaff1b6757ecf300cc92501544
nastiakomarenko <93477647+nastiakomarenko@users.noreply.github.com>
2022-06-30 21:44:08 +0300

    countPositivesSumNegatives

--- src/main/java/com/org/ita/kata/implementation/nastiakomarenko/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/nastiakomarenko/EigthImpl.java
@@ -2,6 +2,8 @@ package com.org.ita.kata.implementation.nastiakomarenko;
 
 import com.org.ita.kata.Eight;
 
+import java.util.Arrays;
+
 public class EigthImpl implements Eight {
     @Override
     public int liters(double time) {
@@ -25,7 +27,24 @@ public class EigthImpl implements Eight {
 
     @Override
     public int[] countPositivesSumNegatives(int[] input) {
+        int res [] = new int[2];
+        int count =0,sum=0;
+        if(input!=null&&input.length>0) {
+            for (int i = 0; i < input.length; i++) {
+                if (input[i] > 0) {
+                    count++;
+                } else {
+                    sum += input[i];
+                }
+            }
+            res[0] = count;
+            res[1] = sum;
+            System.out.println(Arrays.toString(res));
+        }else{
+            System.out.println("0");
+        }
         return new int[0];
+
     }
 
     @Override
