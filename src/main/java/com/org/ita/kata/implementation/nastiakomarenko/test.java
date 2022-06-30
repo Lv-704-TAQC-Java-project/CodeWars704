f61866f7471178acc24cf00a252b3a4a06587f75
nastiakomarenko <93477647+nastiakomarenko@users.noreply.github.com>
2022-06-30 15:26:36 +0300

    remove file

--- src/main/java/com/org/ita/kata/implementation/nastiakomarenko/test.java
+++
@@ -1,9 +0,0 @@
-package com.org.ita.kata.implementation.nastiakomarenko;
-
-public class test {
-    public static void main(String[] args) {
-        double hours = 6.7;
-        double lPerhour = hours * 0.5;
-        System.out.println("time = "+ hours + "---> litres = " + Math.floor(lPerhour));
-    }
-}
