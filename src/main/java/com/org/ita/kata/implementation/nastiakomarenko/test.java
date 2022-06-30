c7ce80a058cf6beec6c21977dae6aaf6c4db7dac
nastiakomarenko <93477647+nastiakomarenko@users.noreply.github.com>
2022-06-30 13:01:32 +0300

    task Keep Hydrated!

---
+++ src/main/java/com/org/ita/kata/implementation/nastiakomarenko/test.java
@@ -0,0 +1,9 @@
+package com.org.ita.kata.implementation.nastiakomarenko;
+
+public class test {
+    public static void main(String[] args) {
+        double hours = 6.7;
+        double lPerhour = hours * 0.5;
+        System.out.println("time = "+ hours + "---> litres = " + Math.floor(lPerhour));
+    }
+}
