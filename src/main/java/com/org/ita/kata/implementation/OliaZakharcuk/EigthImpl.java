5972aac9a93870ca0fa813c661686c1b3af1ae34
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-06-30 13:11:34 +0300

    Implementation of stringToNumber

--- src/main/java/com/org/ita/kata/implementation/OliaZakharcuk/EigthImpl.java
+++ src/main/java/com/org/ita/kata/implementation/OliaZakharcuk/EigthImpl.java
@@ -30,7 +30,8 @@ public class EigthImpl implements Eight {
 
     @Override
     public int stringToNumber(String str) {
-        return 0;
+
+        return Integer.parseInt(str);
     }
 
     @Override
