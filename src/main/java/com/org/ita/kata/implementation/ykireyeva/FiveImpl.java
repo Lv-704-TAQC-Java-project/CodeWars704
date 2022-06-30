05fd392935fe5d2be85aa659187d9e97a9aee00a
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-06-30 12:03:49 +0300

    Implementation of smallest method

--- src/main/java/com/org/ita/kata/implementation/ykireyeva/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/ykireyeva/FiveImpl.java
@@ -3,6 +3,9 @@ package com.org.ita.kata.implementation.ykireyeva;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 public class FiveImpl implements Five {
     @Override
@@ -32,6 +35,47 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
-        return new long[0];
+        ArrayList<Integer> listOfDigits = numIntoDigits(n);
+        StringBuilder s = new StringBuilder();
+        long[] arr = new long[3];
+        int min = Integer.MAX_VALUE;
+        int indexOfMin = 0;
+        int newIndex = 0;
+        int resultNumber;
+        for (int i = 1; i < listOfDigits.size(); i++){
+            if (listOfDigits.get(i) < min){
+                min = listOfDigits.get(i);
+                indexOfMin = i;
+            }
+        }
+        listOfDigits.remove(indexOfMin);
+        if (min == 0 && indexOfMin == 1){
+            indexOfMin = 0;
+            newIndex = 1;
+        }
+        if (min < listOfDigits.get(0)){
+            listOfDigits.add(0, min);
+        } else {
+            listOfDigits.add(1, min);
+            newIndex = 1;
+        }
+
+        listOfDigits.forEach(s::append);
+        resultNumber = Integer.parseInt(s.toString());
+        arr[0] = resultNumber;
+        arr[1] = indexOfMin;
+        arr[2] = newIndex;
+        System.out.println(Arrays.toString(arr));
+        return arr;
+    }
+
+    private ArrayList<Integer> numIntoDigits(long n){
+        ArrayList<Integer> list = new ArrayList<>();
+        while (n > 0){
+            list.add((int) (n % 10));
+            n = n/10;
+        }
+        Collections.reverse(list);
+        return list;
     }
 }
