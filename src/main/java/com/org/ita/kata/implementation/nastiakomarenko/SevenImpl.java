aee9b4a4f2003c9ae755c76df9e115ba2fae8820
nastiakomarenko <93477647+nastiakomarenko@users.noreply.github.com>
2022-07-01 15:27:42 +0300

    newAvg

--- src/main/java/com/org/ita/kata/implementation/nastiakomarenko/SevenImpl.java
+++ src/main/java/com/org/ita/kata/implementation/nastiakomarenko/SevenImpl.java
@@ -6,17 +6,21 @@ public class SevenImpl implements Seven {
     @Override
     public long newAvg(double[] array, double navg) {
 
-         array  =new double[] {14, 30, 5, 7, 9, 11, 15};
-         navg = 92;
-         double sum=0;
-        int count =1;
-        for(int i=0;i<array.length;i++){
-            sum+=array[i];
-            count++;
+        array = new double[]{14, 30, 5, 7, 9, 11, 15};
+        navg = 92;
+        double sum = 0;
+        int count = 1;
+        if (navg <= 0) {
+            throw new IllegalArgumentException();
+        } else {
+            for (int i = 0; i < array.length; i++) {
+                sum += array[i];
+                count++;
+            }
+            double mult = count * navg;
+            double result = mult - sum;
+            return (long) result;
         }
-        double mult = count*navg;
-        double result =mult -sum;
-        return (long) result;
     }
 
     @Override
