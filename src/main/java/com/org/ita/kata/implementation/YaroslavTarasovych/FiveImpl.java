bef960b64c0a5e032cb4ef0c7e1ab650721c1630
Yaroslav Tarasovych <staryk90@gmail.com>
2022-07-01 12:38:49 +0300

    zeros have done!

--- src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/YaroslavTarasovych/FiveImpl.java
@@ -17,7 +17,11 @@ public class FiveImpl implements Five {
 
     @Override
     public int zeros(int n) {
-        return 0;
+        if (n < 5) return 0;
+        int count = 0;
+        for (int i = 5; n / i >= 1; i *= 5)
+            count += n / i;
+        return count;
     }
 
     @Override
@@ -33,5 +37,10 @@ public class FiveImpl implements Five {
     @Override
     public long[] smallest(long n) {
         return new long[0];
+    }
+
+    public static void main(String[] args) {
+        Five five=new FiveImpl();
+        System.out.println(five.zeros(100));
     }
 }
