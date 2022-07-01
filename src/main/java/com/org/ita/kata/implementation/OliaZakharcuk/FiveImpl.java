2cf5587c400bc64ab3d0adb117afdce45f9e8611
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-07-01 23:40:05 +0300

    Implementation of gap

--- src/main/java/com/org/ita/kata/implementation/OliaZakharcuk/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/OliaZakharcuk/FiveImpl.java
@@ -3,7 +3,8 @@ package com.org.ita.kata.implementation.OliaZakharcuk;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
-
+import java.util.ArrayList;
+import java.util.List;
 public class FiveImpl implements Five {
     @Override
     public int artificialRain(int[] v) {
@@ -12,7 +13,40 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+
+        ArrayList<Long> primeNumbers = getPrimeNumbers(m, n);
+        long firstElem = primeNumbers.get(0);
+
+
+        for (long currentElem : primeNumbers) {
+            if (currentElem - firstElem == g) {
+                return new long[]{firstElem, currentElem};
+            } else {
+                firstElem = currentElem;
+            }
+        }
+        return null;
+    }
+    private static boolean isPrime(long n) {
+
+        for (long i = 2; i <= (long) Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static ArrayList<Long> getPrimeNumbers(long m, long n){
+
+        ArrayList<Long> primeNumbers = new ArrayList<>();
+
+        for (long i = m; i <= n; i++) {
+            if (isPrime(i)) {
+                primeNumbers.add(i);
+            }
+        }
+        return primeNumbers;
     }
 
     @Override
