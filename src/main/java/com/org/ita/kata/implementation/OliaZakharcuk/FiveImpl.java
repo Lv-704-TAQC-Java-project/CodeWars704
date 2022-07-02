14cc1bda4aed9a9b87d74b0a8246415f68570053
Olia Zakharchuk <oliaeverest82@gmail.com>
2022-07-02 11:51:55 +0300

    Implementation of solveSum

--- src/main/java/com/org/ita/kata/implementation/OliaZakharcuk/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/OliaZakharcuk/FiveImpl.java
@@ -27,7 +27,9 @@ public class FiveImpl implements Five {
 
     @Override
     public double solveSum(double m) {
-        return 0;
+
+        return  1 + (1 - Math.sqrt(1+4*m)) / (2*m);
+
     }
 
     @Override
