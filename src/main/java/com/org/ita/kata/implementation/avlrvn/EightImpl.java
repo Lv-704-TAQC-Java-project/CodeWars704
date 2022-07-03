4e81baa4acc53ff22b904c2e4f156f45541026f0
avlrvn <87655514+avlrvn@users.noreply.github.com>
2022-07-03 16:12:45 +0300

    Update and rename Volume of a Cuboid to src/main/java/com/org/ita/kata/implementation/avlrvn/EightImpl.java

---
+++ src/main/java/com/org/ita/kata/implementation/avlrvn/EightImpl.java
@@ -0,0 +1,40 @@
+package com.org.ita.kata.implementation.avlrvn;
+import com.org.ita.kata.Eight;
+public class EigthImpl implements Eight {
+    @Override
+    public int liters(double time) {
+        return 0;
+    }
+    @Override
+    public static double getVolumeOfCuboid(final double length, final double width, final double height) {
+    return length * width * height;
+    }
+    @Override
+    public float mpgToKPM(float mpg) {
+        return 0;
+    }
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
