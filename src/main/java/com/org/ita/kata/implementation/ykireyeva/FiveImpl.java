81e42548f1e1cadeb952545f253de6f4ca7188ab
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-07-03 15:33:29 +0300

    Formatting the whole while

--- src/main/java/com/org/ita/kata/implementation/ykireyeva/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/ykireyeva/FiveImpl.java
@@ -3,9 +3,6 @@ package com.org.ita.kata.implementation.ykireyeva;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
-import java.util.ArrayList;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
