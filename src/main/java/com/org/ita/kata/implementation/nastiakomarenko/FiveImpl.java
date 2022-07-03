0c4a16d4fa90e7ee9f182bb905d9c6f4e18ab045
nastiakomarenko <93477647+nastiakomarenko@users.noreply.github.com>
2022-07-03 14:14:23 +0300

    gap

--- src/main/java/com/org/ita/kata/implementation/nastiakomarenko/FiveImpl.java
+++ src/main/java/com/org/ita/kata/implementation/nastiakomarenko/FiveImpl.java
@@ -3,6 +3,8 @@ package com.org.ita.kata.implementation.nastiakomarenko;
 import com.org.ita.kata.Five;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FiveImpl implements Five {
     @Override
@@ -12,7 +14,38 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] gap(int g, long m, long n) {
-        return new long[0];
+
+        List<Long> primes = new ArrayList<>();
+
+        for (long i = m; i < n; i++)
+            if (FiveImpl.prime(i)) {
+                primes.add(i);
+            }
+
+        long[] arr = new long[2];
+
+        for (int j = 0; j < primes.size() - 1; j++) {
+            if ((primes.get(j) + g) == primes.get(j + 1)) {
+                arr[0] = primes.get(j);
+                arr[1] = (primes.get(j + 1));
+                break;
+            }
+        }
+        if (arr[0] == 0 & arr[1] == 0)
+        {
+            arr = null;
+        }
+        return arr;
+        }
+    private static boolean prime(double n) {
+        boolean prime = true;
+        for (int m = 2; m <= n / 2; m++) {
+            if ((n % m) == 0) {
+                prime = false;
+                break;
+            }
+        }
+        return prime;
     }
 
     @Override
@@ -32,6 +65,7 @@ public class FiveImpl implements Five {
 
     @Override
     public long[] smallest(long n) {
+
         return new long[0];
     }
 }
