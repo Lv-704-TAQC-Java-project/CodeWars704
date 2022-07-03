572b4337a461c4a198872dca19eb833b893bf5e8
avlrvn <87655514+avlrvn@users.noreply.github.com>
2022-07-03 16:27:52 +0300

    Update and rename Miles per gallon to src/main/java/com/org/ita/kata/implementation/avlrvn/EigthImpl.java

---
+++ src/main/java/com/org/ita/kata/implementation/avlrvn/EigthImpl.java
@@ -0,0 +1,40 @@
+package com.org.ita.kata.implementation.avlrvn;
+import com.org.ita.kata.Eight;
+public class EigthImpl implements Eight {
+ @Override
+    public int liters(double time) {
+        return 0;
+    }
+    @Override
+    public double getVolumeOfCuboid(double length, double width, double height) {
+        return 0;
+    }
+    @Override
+    public static float mpgToKPM(final float mpg) {
+    return (float)Math.round(100 * mpg * 1.609344 / 4.54609188) / 100;    
+    }
+    @Override
+    public int[] squareOrSquareRoot(int[] array) {
+        return new int[0];
+    }
+    @Override
+    public int[] countPositivesSumNegatives(int[] input) {
+        return new int[0];
+    }
+    @Override
+    public int stringToNumber(String str) {
+        return 0;
+    }
+    @Override
+    public boolean amIWilson(double n) {
+        return false;
+    }
+    @Override
+    public double twoDecimalPlaces(double number) {
+        return 0;
+    }
+    @Override
+    public int[] divisibleBy(int[] numbers, int divider) {
+        return new int[0];
+    }
+}
