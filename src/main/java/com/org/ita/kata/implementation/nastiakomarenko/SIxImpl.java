d94f6869146a01db38d6ebe2b7bac0076a3e1085
nastiakomarenko <93477647+nastiakomarenko@users.noreply.github.com>
2022-07-06 20:09:04 +0300

    final

--- src/main/java/com/org/ita/kata/implementation/nastiakomarenko/SIxImpl.java
+++ src/main/java/com/org/ita/kata/implementation/nastiakomarenko/SIxImpl.java
@@ -3,6 +3,7 @@ package com.org.ita.kata.implementation.nastiakomarenko;
 import com.org.ita.kata.Six;
 
 import java.text.DecimalFormat;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class SIxImpl implements Six {
@@ -109,7 +110,67 @@ public class SIxImpl implements Six {
 
     @Override
     public String nbaCup(String resultSheet, String toFind) {
-        return null;
+
+        String[] pair = resultSheet.split(",");
+        int countOfWin = 0;
+        int countOfDraws = 0;
+        int countOfLost = 0;
+        int totalScore = 0;
+        int totalConceded = 0;
+        int mark = 0;
+
+        for (String i : pair) {
+
+            Pattern teamNamePattern = Pattern.compile("\\b" + toFind + "\\b");
+            Matcher teamNameMatcher = teamNamePattern.matcher(i);
+            Pattern teamScorePattern = Pattern.compile("\\b\\d+\\b");
+            Matcher teamScoreMatcher = teamScorePattern.matcher(i);
+            Pattern wrongScorePattern = Pattern.compile("\\b\\d+\\.\\d+\\b");
+            Matcher wrongScoreMatcher = wrongScorePattern.matcher(i);
+            if (wrongScoreMatcher.find()) return "Error(float number):" + i;
+
+            if (teamNameMatcher.find()) {
+                String[] point = new String[2];
+                int firstTeamScore;
+                int secondTeamScore;
+                int start = teamNameMatcher.start();
+                int j = 0;
+                while (teamScoreMatcher.find()) {
+                    point[j] = i.substring(teamScoreMatcher.start(), teamScoreMatcher.end());
+                    j++;
+                }
+
+                if (start == 0) {
+                    firstTeamScore = Integer.parseInt(point[0]);
+                    secondTeamScore = Integer.parseInt(point[1]);
+                } else {
+                    firstTeamScore = Integer.parseInt(point[1]);
+                    secondTeamScore = Integer.parseInt(point[0]);
+                }
+
+
+                if (firstTeamScore > secondTeamScore) {
+                    countOfWin++;
+                    mark += 3;
+                }
+                if (firstTeamScore == secondTeamScore) {
+                    countOfDraws++;
+                    mark += 1;
+                }
+                if (firstTeamScore < secondTeamScore) countOfLost++;
+                totalScore += firstTeamScore;
+                totalConceded += secondTeamScore;
+            }
+        }
+        if (toFind == null || toFind.length() == 0) return "";
+
+        if ((countOfWin == countOfDraws) && (countOfDraws == countOfLost) && (countOfLost == 0)) {
+            return toFind + ":This team didn't play!";
+
+        } else {
+            return toFind + ":W=" + countOfWin + ";D=" + countOfDraws + ";L=" + countOfLost + ";Scored=" + totalScore + ";Conceded=" + totalConceded + ";Points=" + mark;
+
+        }
     }
 
     @Override
