5c8a686c8d30d1fddad228fb9cd45c0c1af0b714
DankoMaksym <dankomaksym@gmail.com>
2022-07-07 21:12:10 +0300

    dankomax - use getValidIntFromUserInput in implementation selection

--- src/main/java/com/org/ita/utils/ListImplementation.java
+++ src/main/java/com/org/ita/utils/ListImplementation.java
@@ -23,7 +23,7 @@ public class ListImplementation implements Runner{
         colorln("0 - return to main manu", ANSI_YELLOW);
         colorln("ID from 1 to " + Group.values().length + " - select imlementation", ANSI_GREEN);
 
-        int a = br.readInt();
+        int a = br.getValidIntFromUserInput("Invalid input! Number should be in range from 1 to 24.", 0, 9);
         if (a > 0 && a <= Group.values().length) {
             SettingsMenu.setSetImplementation(a);
         }
