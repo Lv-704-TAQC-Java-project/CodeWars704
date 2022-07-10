6eff8dfb4b675ba158fd4e17e1669cca462be144
Jaroslav Tarasovych <staryk90@gmail.com>
2022-07-11 00:35:05 +0300

    experiment!

---
+++ src/test/java/com/org/ita/kata/implementation/Eight/SquareOrSquareRootTest.java
@@ -0,0 +1,52 @@
+package com.org.ita.kata.implementation.Eight;
+
+import com.org.ita.kata.Eight;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+import java.util.Arrays;
+
+
+public class SquareOrSquareRootTest {
+    @DataProvider(name = "classes")
+    public Object[][] dataProviderFunc(){
+        return new Object[][]{{ new com.org.ita.kata.implementation.avlrvn.EigthImpl(), "avlrvn"},
+                {new com.org.ita.kata.implementation.clarmmym.EigthImpl(), "clarmmym"},
+                {new com.org.ita.kata.implementation.dankomax.EigthImpl(), "dankomax"},
+                {new com.org.ita.kata.implementation.MyroslavaVynokur.EigthImpl(), "MyroslavaVynokur"},
+                {new com.org.ita.kata.implementation.nastiakomarenko.EigthImpl(), "nastiakomarenko"},
+                {new com.org.ita.kata.implementation.OlhaPivniuk.EigthImpl(), "OlhaPivniuk"},
+                {new com.org.ita.kata.implementation.OliaZakharcuk.EigthImpl(), "OliaZakharcuk"},
+                {new com.org.ita.kata.implementation.YaroslavTarasovych.EigthImpl(), "YaroslavTarasovych"},
+                {new com.org.ita.kata.implementation.ykireyeva.EigthImpl(), "ykireyeva"}};
+    }
+
+    @Test(dataProvider = "classes")
+    public void Test1(Eight eight, String implementation){
+            Eight work = eight;
+            int[] input = new int[] { 4, 3, 9, 7, 2, 1 };
+            int[] expected = new int[] { 2, 9, 3, 49, 4, 1 };
+            Assert.assertEquals(Arrays.toString(work.squareOrSquareRoot(input)), Arrays.toString(expected), "Error test 1 "+implementation);
+
+    }
+
+    @Test(dataProvider = "classes")
+    public void Test2(Eight eight, String implementation){
+        Eight work = eight;
+
+        int[] input = new int[] { 100, 101, 5, 5, 1, 1 };
+        int[] expected = new int[] { 10, 10201, 25, 25, 1, 1 };
+        Assert.assertEquals(Arrays.toString(work.squareOrSquareRoot(input)), Arrays.toString(expected), "Error test 2 "+implementation);
+
+    }
+
+    @Test(dataProvider = "classes")
+    public void Test3(Eight eight, String implementation){
+        Eight work = eight;
+
+        int[] input = new int[] { 1, 2, 3, 4, 5, 6 };
+        int[] expected = new int[] { 1, 4, 9, 2, 25, 36 };
+        Assert.assertEquals(Arrays.toString(work.squareOrSquareRoot(input)), Arrays.toString(expected), "Error test 3 "+implementation);
+    }
+}
---
+++ src/test/java/com/org/ita/kata/implementation/Seven/WhereIsHeTest.java
@@ -0,0 +1,46 @@
+package com.org.ita.kata.implementation.Seven;
+import com.org.ita.kata.Seven;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+public class WhereIsHeTest {
+    @DataProvider(name = "classes")
+    public Object[][] dataProviderFunc(){
+        return new Object[][]{{ new com.org.ita.kata.implementation.avlrvn.SevenImpl(), "avlrvn"},
+                {new com.org.ita.kata.implementation.clarmmym.SevenImpl(), "clarmmym"},
+                {new com.org.ita.kata.implementation.dankomax.SevenImpl(), "dankomax"},
+                {new com.org.ita.kata.implementation.MyroslavaVynokur.SevenImpl(), "MyroslavaVynokur"},
+                {new com.org.ita.kata.implementation.nastiakomarenko.SevenImpl(), "nastiakomarenko"},
+                {new com.org.ita.kata.implementation.OlhaPivniuk.SevenImpl(), "OlhaPivniuk"},
+                {new com.org.ita.kata.implementation.OliaZakharcuk.SevenImpl(), "OliaZakharcuk"},
+                {new com.org.ita.kata.implementation.YaroslavTarasovych.SevenImpl(), "YaroslavTarasovych"},
+                {new com.org.ita.kata.implementation.ykireyeva.SevenImpl(), "ykireyeva"}};
+    }
+
+    @Test(dataProvider = "classes")
+    public void Test1(Seven seven, String implementation){
+        Seven work = seven;
+        int[] input = new int[] { 3, 1, 1};
+        int expected = 2;
+        Assert.assertEquals(work.whereIsHe(input[0], input[1], input[2]), expected, "Error test 1 "+implementation);
+    }
+
+    @Test(dataProvider = "classes")
+    public void Test2(Seven seven, String implementation){
+        Seven work = seven;
+        int[] input = new int[] { 5, 2, 3};
+        int expected = 3;
+        Assert.assertEquals(work.whereIsHe(input[0], input[1], input[2]), expected, "Error test 2 "+implementation);
+    }
+
+    @Test(dataProvider = "classes")
+    public void Test3(Seven seven, String implementation){
+        Seven work = seven;
+        int[] input = new int[] { 6, 2, 3};
+        int expected = 4;
+        Assert.assertEquals(work.whereIsHe(input[0], input[1], input[2]), expected, "Error test 3 "+implementation);
+
+    }
+}
