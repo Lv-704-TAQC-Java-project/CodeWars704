2ca00c64ceb13d080646849ff3c3f63229fbaf89
DankoMaksym <dankomaksym@gmail.com>
2022-07-11 00:03:59 +0300

    dankomax - refactor EightImplTest class

--- src/test/java/unit_test/EigthImplTest.java
+++ src/test/java/unit_test/EigthImplTest.java
@@ -2,25 +2,12 @@ package unit_test;
 
 import com.org.ita.kata.Eight;
 import org.testng.Assert;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
-public class EigthImplTest {
-    public Eight[] IMPLEMENTATIONS = new Eight[]{
-            new com.org.ita.kata.implementation.avlrvn.EigthImpl(),
-            new com.org.ita.kata.implementation.clarmmym.EigthImpl(),
-            new com.org.ita.kata.implementation.dankomax.EigthImpl(),
-            new com.org.ita.kata.implementation.MyroslavaVynokur.EigthImpl(),
-            new com.org.ita.kata.implementation.nastiakomarenko.EigthImpl(),
-            new com.org.ita.kata.implementation.OlhaPivniuk.EigthImpl(),
-            new com.org.ita.kata.implementation.OliaZakharcuk.EigthImpl(),
-            new com.org.ita.kata.implementation.YaroslavTarasovych.EigthImpl(),
-            new com.org.ita.kata.implementation.ykireyeva.EigthImpl()
-    };
+public class EigthImplTest extends BasicImplTest {
 
     @DataProvider(name = "amIWilsonTestData")
     public Object[][] amIWilsonTestData() {
-
         Object[][] baseTestData = new Object[][]{
                 {true, 5.0},
                 {true, 13.0},
@@ -41,23 +28,12 @@ public class EigthImplTest {
                 {false, 530},
                 {false, 384},
         };
-
-        int fullTestDataLength = IMPLEMENTATIONS.length * baseTestData.length;
-        Object[][] fullTestData = new Object[fullTestDataLength][3];
-
-        int count = 0;
-        for (Eight implementation : IMPLEMENTATIONS) {
-            for (Object[] baseTest : baseTestData) {
-                fullTestData[count] = new Object[]{implementation, baseTest[0], baseTest[1]};
-                count++;
-            }
-        }
-
-        return fullTestData;
+        return userImpl.combineEightWithTestData(baseTestData);
     }
 
     @Test(dataProvider = "amIWilsonTestData")
-    public void amIWilsonTest(Eight impl, boolean result, double number) {
-        Assert.assertEquals(impl.amIWilson(number), result);
+    public void amIWilsonTest(Eight impl, boolean expected, double number) {
+        boolean actual = impl.amIWilson(number);
+        Assert.assertEquals(actual, expected);
     }
 }
