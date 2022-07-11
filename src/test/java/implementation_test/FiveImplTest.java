3018a06223a2d2a6a504963895791139e766fb2a
Yaryna_Kireyeva <yarynakireyeva@gmail.com>
2022-07-11 17:14:32 +0300

    Implementation of testZeros

--- src/test/java/implementation_test/FiveImplTest.java
+++ src/test/java/implementation_test/FiveImplTest.java
@@ -13,4 +13,10 @@ public class FiveImplTest {
         Assert.assertEquals(actual, expected, 1e-13);
     }
 
+    @Test(dataProvider = "data-provider-zeros", dataProviderClass =  FiveData.class, timeOut = 1000)
+    public void testZeros(Five fiveImpl, int data, int expected) {
+        int actual = fiveImpl.zeros(data);
+        Assert.assertEquals(actual, expected);
+    }
+
 }
