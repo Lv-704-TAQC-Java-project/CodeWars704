3d3ce4f1bcfddacc7e873e106d4be45051539dd8
Jaroslav Tarasovych <staryk90@gmail.com>
2022-07-12 23:38:21 +0300

    some changes!

--- src/test/java/utils/data_provider_buffer/BufferData.java
+++ src/test/java/utils/data_provider_buffer/BufferData.java
@@ -6,7 +6,7 @@ public class BufferData {
     @DataProvider(name = "data-provider-readIntArr")
     public static Object[][] testReadIntArr() {
         return new Object[][]{
-                {"1 2 3 4", new int[]{1, 2, 0, 4}},
+                {"1 2 3 4", new int[]{1, 2, 3, 4}},
                 {"1 8 3 4", new int[]{1, 8, 3, 4}}
         };
     }
