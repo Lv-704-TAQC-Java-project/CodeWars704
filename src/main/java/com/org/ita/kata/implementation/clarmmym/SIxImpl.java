8c529c326bf90c11563b4a4d44384c5bd715a187
Olenka <olenkadashinich@gmail.com>
2022-07-12 22:01:43 +0300

    fixed method stock summary

--- src/main/java/com/org/ita/kata/implementation/clarmmym/SIxImpl.java
+++ src/main/java/com/org/ita/kata/implementation/clarmmym/SIxImpl.java
@@ -192,6 +192,10 @@ public class SIxImpl implements Six {
     public String stockSummary(String[] lstOfArt, String[] lstOf1stLetter) {
         StringBuilder result = new StringBuilder();
 
+        if (lstOfArt.length == 0 || lstOf1stLetter.length == 0) {
+            return "";
+        }
+
         for (var firstLetter : lstOf1stLetter) {
             int temp = 0;
             for (var dataOfBook : lstOfArt) {
