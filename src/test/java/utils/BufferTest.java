b634d74e82c3d70e7a4c342addf83a13cfe3173d
MyroslavaVynokur <mirka.vyno@gmail.com>
2022-07-12 22:59:57 +0300

    add testReadString

--- src/test/java/utils/BufferTest.java
+++ src/test/java/utils/BufferTest.java
@@ -1,12 +1,24 @@
 package utils;
 
+import com.org.ita.utils.Buffer;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import utils.data_provider_buffer.BufferData;
 
-import static org.testng.Assert.*;
+import java.io.*;
 
 public class BufferTest {
 
-    @Test
-    public void testReadString() {
+    public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    @Test (dataProvider = "data-provider-readString", dataProviderClass = BufferData.class)
+    public void testReadString(String input, String expected) {
+        System.setIn(new ByteArrayInputStream(input.getBytes()));
+        Buffer br = new Buffer();
+        OutputStream output = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(output));
+        String actual = br.readString();
+        Assert.assertEquals(actual, expected);
     }
+
 }
--- src/test/java/utils/data_provider_buffer/BufferData.java
+++ src/test/java/utils/data_provider_buffer/BufferData.java
@@ -1,4 +1,13 @@
 package utils.data_provider_buffer;
 
+import org.testng.annotations.DataProvider;
+
 public class BufferData {
+
+    @DataProvider(name = "data-provider-readString")
+    public static Object[][] testReadString() {
+        return new Object[][]{
+                {"ATQC course", "ATQC course"}
+        };
+    }
 }
