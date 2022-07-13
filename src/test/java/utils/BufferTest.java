e0777085b1800a9cca357d52568ed81bad442411
Jaroslav Tarasovych <staryk90@gmail.com>
2022-07-13 17:34:14 +0300

    some changes!

--- src/test/java/utils/BufferTest.java
+++ src/test/java/utils/BufferTest.java
@@ -6,6 +6,9 @@ import org.testng.annotations.Test;
 import utils.data_provider_buffer.BufferData;
 
 import java.io.*;
+import java.math.BigInteger;
+
+import static org.testng.Assert.assertThrows;
 
 public class BufferTest {
 
@@ -24,6 +27,21 @@ public class BufferTest {
         Buffer bf = new Buffer();
         long actual = bf.readLong();
         Assert.assertEquals(actual, expected);
+    }
+
+    @Test(dataProvider = "readBigIntegerTestData", dataProviderClass = BufferData.class)
+    public void testReadBigInteger(String input, BigInteger expected) {
+        System.setIn(new ByteArrayInputStream((input).getBytes()));
+        Buffer bf = new Buffer();
+        BigInteger actual = bf.readBigInteger();
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test( expectedExceptions = {NumberFormatException.class, NullPointerException.class}, dataProvider = "readBigIntegerNegativeTestData", dataProviderClass = BufferData.class)
+    public void testReadBigIntegerNegative(String input) {
+        System.setIn(new ByteArrayInputStream((input).getBytes()));
+        Buffer bf = new Buffer();
+        bf.readBigInteger();
     }
 
 }
